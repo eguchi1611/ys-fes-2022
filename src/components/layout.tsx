3a29627998234e320bdac4560cb9f8b2a55fdfca
ITO Keita <kk79it@gmail.com>
2022-05-05 17:44:51 +0900

    レイアウトの作成

---
+++ src/components/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactElement } from "react";
+import Header from "./header";
+
+type Props = {
+  children: ReactElement;
+};
+
+function Layout({ children }: Props): ReactElement {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
