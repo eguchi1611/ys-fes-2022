6d47fb50cc7adbbc23535d04c7d841e52c28f48b
ITO Keita <kk79it@gmail.com>
2022-05-05 23:45:53 +0900

    ヘッダーの調整

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const links = [
+  { text: "ホーム", href: "/" },
+  { text: "問い合わせ", href: "/info" },
+];
+
 function Header() {
   return (
-    <header className="flex px-4 py-2 items-center">
-      <div className="relative h-16 w-48">
-        <Image objectFit="contain" layout="fill" src="/logo.jpg" />
-      </div>
-      {/* <nav>
+    <header className="flex items-center px-4 py-2">
+      <Link href="/">
+        <div className="relative mr-auto h-16 w-44 cursor-pointer">
+          <Image objectFit="contain" layout="fill" src="/logo.jpg" priority />
+        </div>
+      </Link>
+      <nav>
         <ul className="flex">
-          <li>
-            <Link href="/">ホーム</Link>
-          </li>
-          <li>
-            <Link href="/">ホーム</Link>
-          </li>
+          {links.map(({ text, href }) => (
+            <li key={text} className="px-2">
+              <Link href={href}>{text}</Link>
+            </li>
+          ))}
         </ul>
-      </nav> */}
+      </nav>
     </header>
   );
 }
