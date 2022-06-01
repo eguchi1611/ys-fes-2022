54fea26347e031c7dedd257fe60f0305efe1af88
ITO Keita <kk79it@gmail.com>
2022-06-02 08:45:42 +0900

    タイトルを変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <title>八千代松陰文化祭</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
     </Layout>
   );
 }
