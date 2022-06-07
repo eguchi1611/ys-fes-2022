3bd2cfefd39ec9ea6b330ab3f99f4362442c236c
ITO Keita <kk79it@gmail.com>
2022-06-07 13:44:35 +0900

    _document.tsxの作成

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <p>sss</p>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
