1124b7ed4ce41c9b17f7bf71e9e5a2e1acf07fc8
ITO Keita <kk79it@gmail.com>
2022-06-13 00:38:03 +0900

    公開終了メッセージの追加

--- src/pages/highschool/1st.tsx
+++ src/pages/highschool/1st.tsx
@@ -15,6 +15,9 @@ const Page1st: NextPage<Props> = ({ sections }) => {
       <Head>
         <title>高校１年 - 八千代松陰文化祭</title>
       </Head>
+      <div className="text-center py-5">
+        動画の公開は終了しました。ご視聴ありがとうございました。
+      </div>
       <Template sections={sections} />
     </>
   );
