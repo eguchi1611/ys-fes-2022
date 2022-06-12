fd8eca608977e8185799af5bf8e2a54e1ca7dba5
ITO Keita <kk79it@gmail.com>
2022-06-12 12:08:58 +0900

    コンテンツの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,67 @@
 import type { NextPage } from "next";
+import Card from "../components/card";
 
 const Home: NextPage = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row row-cols-1 row-cols-lg-2">
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+      </div>
+      <p>a</p>
+      <p>a</p>
+      <p>a</p>
+    </div>
+  );
 };
 
 export default Home;
