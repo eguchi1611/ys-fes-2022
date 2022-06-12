7e9bbe45fefe58946e5e7c90314b0aa89b534ede
ITO Keita <kk79it@gmail.com>
2022-06-12 12:08:17 +0900

    レイアウトの作成

---
+++ src/components/layout.tsx
@@ -0,0 +1,27 @@
+import classNames from "classnames";
+import { ReactElement } from "react";
+import style from "../styles/layout.module.scss";
+import Footer from "./footer";
+import Header from "./header";
+
+type Props = {
+  children: ReactElement;
+};
+
+function Layout({ children }: Props) {
+  return (
+    <div>
+      <div className="position-relative">
+        <Header />
+      </div>
+      <div className={classNames(style.main, "min-vh-100 position-relative")}>
+        {children}
+      </div>
+      <div className="container-md">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
