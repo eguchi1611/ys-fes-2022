8da390ee6ef6e81d9d617f1bdfb79d7ba1d2296c
ITO Keita <kk79it@gmail.com>
2022-06-13 00:31:55 +0900

    Bootstrapスクリプトの変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Layout from "../components/layout";
 import "../styles/custom.scss";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossOrigin="anonymous"
+      ></Script>
+    </>
   );
 }
 
