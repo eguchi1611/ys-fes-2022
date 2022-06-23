88e7314536c0b7fc65d801811b2a12abd764a345
ITO Keita <kk79it@gmail.com>
2022-06-23 23:01:18 +0900

    チャットの作成

--- src/components/rabbit.tsx
+++ src/components/rabbit.tsx
@@ -2,17 +2,12 @@ import { useEffect, useState } from "react";
 import { Image, Layer, Stage } from "react-konva";
 import useImage from "use-image";
 
-type Props = {
-  handleClick: () => void;
-};
-
 const useAnimationTimer = () => {
   const [score, setScore] = useState(0);
   useEffect(() => {
     const timer = setInterval(() => {
       setScore((c) => c + 1);
-      console.log("run");
-    }, 200);
+    }, 280);
     return () => {
       clearInterval(timer);
     };
@@ -21,7 +16,7 @@ const useAnimationTimer = () => {
   return score;
 };
 
-function SeishunRabbit({ handleClick }: Props) {
+function SeishunRabbit() {
   const [walking1] = useImage("/images/rabbits/walking1.png");
   const [walking2] = useImage("/images/rabbits/walking2.png");
 
@@ -36,8 +31,6 @@ function SeishunRabbit({ handleClick }: Props) {
           image={animationFrame % 2 === 0 ? walking1 : walking2}
           width={90}
           height={110}
-          onClick={handleClick}
-          onTouchEnd={handleClick}
           shadowColor="black"
           shadowBlur={5}
           shadowOffset={{ x: 3, y: 3 }}
