776f34638c5f75f718f11f09778c61ea120e9db8
ITO Keita <kk79it@gmail.com>
2022-06-23 23:01:18 +0900

    チャットの作成

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -4,11 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import style from "../styles/chat.module.scss";
 
-type Props = {
-  handleClose: () => void;
-};
-
-function Chat({ handleClose }: Props) {
+function Chat() {
   const [messages, setMessages] = useState<Message[]>([]);
   const addMessage = (message: Message) => {
     setMessages((c) => c.concat(message));
@@ -16,9 +12,21 @@ function Chat({ handleClose }: Props) {
 
   const chatWindowRef = useRef<HTMLDivElement>(null);
 
-  const { register, handleSubmit, reset } = useForm<FormInputs>();
+  const { register, handleSubmit, reset, setFocus } = useForm<FormInputs>();
+
+  const scroll = () => {
+    // １番下にスクロール
+    if (chatWindowRef.current) {
+      chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
+    }
+  };
 
   const onSubmit = (data: FormInputs) => {
+    // 空白だったらキャンセル
+    if (!data.text) {
+      return;
+    }
+
     // 入力を削除
     reset();
 
@@ -34,20 +42,98 @@ function Chat({ handleClose }: Props) {
         const reply = res.status === 200 ? res.data : "わかんないよ〜！";
         addMessage({ content: reply, sender: "response" });
 
-        // １番下にスクロール
-        if (chatWindowRef.current) {
-          chatWindowRef.current.scrollTop = chatWindowRef.current.scrollHeight;
-        }
+        scroll();
       })
-      .catch((error) => {
-        console.log(error);
-      });
+      .catch(console.log);
   };
 
   useEffect(() => {
-    setMessages([{ content: "なんでも質問してね〜！", sender: "response" }]);
+    const history = localStorage.getItem("chat_history");
+    if (history) {
+      setMessages(JSON.parse(history));
+    } else {
+      setMessages([{ content: "なんでも質問してね〜！", sender: "response" }]);
+    }
   }, []);
 
+  useEffect(() => {
+    localStorage.setItem("chat_history", JSON.stringify(messages));
+  }, [messages]);
+
+  useEffect(() => {
+    var modal = document.getElementById("chatModal");
+    if (modal) {
+      modal.addEventListener("shown.bs.modal", function (event) {
+        setFocus("text");
+        scroll();
+      });
+    }
+  }, []);
+
+  return (
+    <div className="modal fade" id="chatModal" tabIndex={-1}>
+      <div className="modal-dialog modal-dialog-scrollable modal-fullscreen-sm-down">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title"></h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+            ></button>
+          </div>
+          <div className="modal-body" ref={chatWindowRef}>
+            <div style={{ height: 360, maxHeight: "80%" }}>
+              {messages.map(({ content, sender }, index) => (
+                <div
+                  key={index}
+                  className={classNames("d-flex align-items-center", {
+                    "flex-row-reverse": sender === "request",
+                  })}
+                >
+                  {sender === "request" || (
+                    <img
+                      style={{ transform: "scale(-1, 1)" }}
+                      src="/images/rabbits/walking1.png"
+                      height={42}
+                    />
+                  )}
+                  <p
+                    className={classNames("rounded-3", style.message, {
+                      ["text-end"]: sender === "request",
+                      [style.request]: sender === "request",
+                      [style.response]: sender === "response",
+                    })}
+                  >
+                    {content}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="modal-footer">
+            <form
+              className="input-group input-group-sm"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <input
+                type="text"
+                className="form-control"
+                placeholder="知りたいことを入力してください"
+                autoComplete="off"
+                {...register("text")}
+              />
+              <button className="btn btn-outline-secondary" type="submit">
+                送信
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  /*
   return (
     <div className="card shadow-sm m-3" style={{ width: 360 }}>
       <div className="card-header d-flex">
@@ -105,6 +191,7 @@ function Chat({ handleClose }: Props) {
       </form>
     </div>
   );
+  */
 }
 
 export default Chat;
