9b24f31ea9026e42a0f74633e669bafbf070208d
ITO Keita <kk79it@gmail.com>
2022-06-23 22:59:00 +0900

    インデックスファイルを更新

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,12 +1,18 @@
 type Section = {
-  title: string;
-  subtitle: string;
-  description: string;
-  bannerUrl: string;
-  movies: Movies[];
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  bannerUrl?: string;
+  movies?: Movies[];
+  localUrls?: LocalURL[];
 };
 
 type Movies = {
   title: string;
+  url: string;
+};
+
+type LocalURL = {
+  text: string;
   url: string;
 };
